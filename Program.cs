5773a1e73f85cc3d78392d8cb5d772d17c04ad04
iremhatipoglu <62469438+iremhatipoglu@users.noreply.github.com>
2022-02-15 10:24:24 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace loopPatika
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lüften sayı giriniz ");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= a; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    Console.WriteLine(i);
+
+                }
+
+
+            }
+            Console.WriteLine("********************");
+            Console.WriteLine("Yeni Örnek");
+
+            int tek = 0;
+            int cift = 0;
+            for (int i = 0; i <= 1000; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    tek += i;
+                }
+
+                else
+                {
+                    cift += i;
+                }
+
+            }
+            Console.WriteLine("Tek sayılar toplamı :" + tek);
+            Console.WriteLine("Çift sayılar toplamı :" + cift);
+
+            Console.WriteLine("Yeni Örnek");
+
+            for (int i = 1; i < 10; i++)
+            {
+                if (i == 5)
+                    break;
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("Yeni Örnek");
+            for (int i = 1; i < 10; i++)
+            {
+                if (i == 5)
+                    continue;
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
